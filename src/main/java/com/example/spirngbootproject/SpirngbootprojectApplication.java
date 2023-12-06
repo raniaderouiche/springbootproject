2fc7c877a34aaee23daa11a634fd0d7c04480077
raniaderouiche <rania.derouiche@gmail.com>
2023-12-06 21:57:02 +0100

    first commit

---
+++ src/main/java/com/example/spirngbootproject/SpirngbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirngbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngbootprojectApplication.class, args);
+    }
+
+}
